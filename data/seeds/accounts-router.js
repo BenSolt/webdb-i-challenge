0810146388bdd5b3ea0b06bcbb874ed4c758c411
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-20 17:56:02 -0500

    added router file with crud

---
+++ data/seeds/accounts-router.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+const AccInfo = require('../dbConfig');
+
+router.get("/", asnyc (req, res, next) => {
+    try{
+
+    }
+})
+
+router.get("/:id", asnyc (req, res, next) => {
+    try{
+
+    }
+})
+
+router.post("/", asnyc (req, res, next) => {
+    try{
+
+    }
+})
+
+router.put("/:id", asnyc (req, res, next) => {
+    try{
+
+    }
+})
+
+router.delete("/:id", asnyc (req, res, next) => {
+    try{
+
+    }
+})
+
+module.exports = router
