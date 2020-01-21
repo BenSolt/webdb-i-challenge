1b95150a8ed0ab0540516299f4d44e074384a03c
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-20 21:21:27 -0500

    filled out

--- data/seeds/accounts-router.js
+++ data/seeds/accounts-router.js
@@ -16,7 +16,8 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
     try{
-       res.json(await db.select("*").from("posts").where("id", req.params.id))
+       res.json(await db.select("*").from("posts")
+       .where("id", req.params.id))
     }catch (err){
         next(err)
     }
@@ -24,7 +25,16 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
     try{
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+        //translates INSERT INTO posts (title, contents) VALUES(?,?);
 
+        //const [id] = await db.select("*").from("posts").insert(payload)
+        const [id] = await db("posts").insert(payload)
+        res.json(await db ("posts").where("id", id).first())
+        
     }catch (err){
         next(err)
     }
@@ -32,7 +42,13 @@ router.post("/", async (req, res, next) => {
 
 router.put("/:id", async (req, res, next) => {
     try{
-
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+        //translate to UPDATE posts SET title = ? AND contenst = ? WHERE id = ?;
+        await db("posts").where("id", req.params.id).update(payload)
+        res.json(await db ("posts").where("id", req.params.id).first())
     }catch (err){
         next(err)
     }
@@ -40,7 +56,8 @@ router.put("/:id", async (req, res, next) => {
 
 router.delete("/:id", async (req, res, next) => {
     try{
-
+        await db("posts").where("id", req.params.id).del()
+        res.status(204).end()
     } catch (err) {
         next(err)
     }
